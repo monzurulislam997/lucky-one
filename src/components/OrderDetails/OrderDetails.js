93bbc8b9557fe3b3bb2a3abfd5fb8ddf82a6cc17
fname lname <monzurulislam997@gmail.com>
2022-05-21 22:47:48 +0600

    choose one done

--- src/components/OrderDetails/OrderDetails.js
+++ src/components/OrderDetails/OrderDetails.js
@@ -1,21 +1,38 @@
 import React from 'react';
+import { useState } from 'react';
 import AllOrderDetails from '../AllOrderDetails/AllOrderDetails';
+import './OrderDetails.css'
 
 const OrderDetails = (props) => {
     
     const singleWatches=props.singleWatch;
+     const [choose,setChoose]=useState({})
     if(singleWatches.length>4){
        singleWatches.length=4;
        return alert("You can't add more than 4")
        
     }
-    // const {name,price,img}=props.singleWatch;
-    // singleWatches=
-
+    const chooseOneHandle=()=>{
+        
+        const min = 1;
+        const max = 4;
+        const intNumber = Math.floor(Math.random() * (max - min)) + min;
+        console.log(intNumber)
+         setChoose(singleWatches[intNumber])
+     
+        //   singleWatches.length=0;
+        
+    }
+   
     
     return (
         <div>
-            
+                
+                <div className='choose-watch'>
+                        <img src={choose?.img} alt=""  />
+                   <h2 style={{color:"green"}}>{choose?.name} </h2>
+                   </div>
+                
 
                 {
                     singleWatches.map(watch=><AllOrderDetails
@@ -25,8 +42,8 @@ const OrderDetails = (props) => {
                          )
                 }
 
-         <button>CHOOSE 1 FOR ME</button> <br />
-         <button> CHOOSE AGAIN</button>
+         <button onClick={chooseOneHandle} className='choose-btn'>CHOOSE 1 FOR ME</button> <br />
+         <button className='reset-btn'> CHOOSE AGAIN</button>
 
         </div>
     );
